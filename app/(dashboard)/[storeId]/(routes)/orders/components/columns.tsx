059abf1f7f868e0180a791057adc1ffe68172459
Nishadchowdhury <Nishadhj111@gmail.com>
2024-04-22 13:42:39 +0600

    build time error fixed

--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { CellAction } from "./cell-action";
 import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
